90110732fd5f8ca7844665e3f58eda6c07e5ba9e
JayanthJake <jayanthsapiensj2.71828@gmail.com>
2018-02-27 22:07:34 +0530

    GEORGE MICHAEL

---
+++ george_michael.py
@@ -0,0 +1,8 @@
+george = True
+michael = 1
+while george:
+    if michael == 1:
+        print("George Michael was one of the greatest singers who ever lived")
+        break
+    else:
+        print("The computer is stoned")
